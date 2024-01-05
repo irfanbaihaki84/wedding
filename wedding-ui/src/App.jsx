a8ec5dee1a8e9ebffeda0930cbe5a1b530c68d1c
irfanbaihaki84 <coyrikbae@gmail.com>
2024-01-05 17:38:49 +0700

    baihaki

--- wedding-ui/src/App.jsx
+++ wedding-ui/src/App.jsx
@@ -7,46 +7,67 @@ import namastee from './assets/namastee-hand.png';
 function App() {
   return (
     <div className="container">
-      <section className="one">
+      <section className="home">
         <h2>SENIN, 20 NOVEMBER 2024</h2>
         <h2>WE INVITE YOU</h2>
         <h2>TO OUR WEDDING</h2>
-        <h1>Romeo and Juliet</h1>
+        <h1 className="title-1">Romeo and Juliet</h1>
         <h2>Hello, We Are Getting Married</h2>
-        <div className="one-1">
+        <div className="home-1">
           <h2>Dear</h2>
-          <h2>Anya</h2>
+          <h2 className="title-2">Anya</h2>
           <h2>Tangerang</h2>
           <button>Open Invitation</button>
         </div>
       </section>
-      <section className="two">
-        <h1>COUNTDOWN</h1>
+
+      <section className="countdown">
+        <h1 className="title-2">
+          COUNTDOWN <span className="material-symbols-outlined">timer</span>
+        </h1>
         <h2>Days : Hours : Minutes : Seconds</h2>
-        <div className="two-1">
-          <h1>NOTES</h1>
+        <div className="countdown-1">
+          <h1 className="title-2">
+            NOTES <span className="material-symbols-outlined">description</span>
+          </h1>
           <h2>Protokol Covid-19</h2>
           <h3>Dalam upaya mengurangi penyebaran Covid-19 pada masa pandemi,</h3>
           <h3>
             kami harapkan kedatangan para tamu undangan agar menjalankan
             protocol yang berlaku di tempat.
           </h3>
-          {/* <span className="material-symbols-outlined">medical_mask</span> */}
+
           <img src={maskMedic} alt="mask-man" />
-          <h3>Menggunakan Masker</h3>
-          {/* <span className="material-symbols-outlined">social_distance</span> */}
+          <h3>
+            Menggunakan Masker
+            <span className="material-symbols-outlined">medical_mask</span>
+          </h3>
+
           <img src={keepDistance} alt="keep-distance" />
-          <h3>Menjaga Jarak</h3>
-          {/* <span className="material-symbols-outlined">cheer</span> */}
+          <h3>
+            Menjaga Jarak{' '}
+            <span className="material-symbols-outlined">social_distance</span>
+          </h3>
+
           <img src={namastee} alt="namastee-hand" />
-          <h3>Mengunakan salam namastee sebagai pangganti jabat tangan</h3>
-          {/* <span className="material-symbols-outlined">clean_hands</span> */}
+          <h3>
+            Mengunakan salam namastee sebagai pangganti jabat tangan{' '}
+            <span className="material-symbols-outlined">cheer</span>
+          </h3>
+
           <img src={handWash} alt="hand-wash" />
-          <h3>Menjaga kebersihan dengan mencuci tangan atau handsanitizer</h3>
+          <h3>
+            Menjaga kebersihan dengan mencuci tangan atau handsanitizer{' '}
+            <span className="material-symbols-outlined">clean_hands</span>
+          </h3>
         </div>
       </section>
-      <section className="three">
-        <h1>BRIDE & GROOM</h1>
+
+      <section className="bridegroom">
+        <h1 className="title-2">
+          BRIDE & GROOM{' '}
+          <span className="material-symbols-outlined">favorite</span>
+        </h1>
         <h3>Assalamu'alaikum Warahmatullahi Wabarakatuh</h3>
         <h3>
           Maha suci Allah SWT yang telah menciptakan mahluk-Nya
@@ -57,8 +78,21 @@ function App() {
           di antara Putra-Putri kami :
         </h3>
       </section>
-      <section className="four">
-        <h1>GALLERY</h1>
+
+      <section className="gallery">
+        <h1 className="title-2">
+          GALLERY{' '}
+          <span className="material-symbols-outlined">photo_camera</span>
+        </h1>
+      </section>
+
+      <section className="gift">
+        <h1 className="title-2">
+          GIFT{' '}
+          <span className="material-symbols-outlined">
+            featured_seasonal_and_gifts
+          </span>
+        </h1>
       </section>
     </div>
   );
